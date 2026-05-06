4f4996180116a8c44395b640813677201069cc1c
Abigael Ongwae <abbykerry17@gmail.com>
2026-05-06 21:49:49 +0300

    first structure, main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+# This is Pydantic (our "gatekeeper")
+class Farmer(BaseModel):
+    full_name: str = Field(..., min_length=3) # The "..." means this field is required
+    phone_number: str = Field(..., min_length=10, max_length=13)
+    cluster: str | None = None # Optional field, can be None
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello farmers!"}
+
+
+@app.post("/farmers")
+def create_farmer(farmer: Farmer):
+    return {
+        "message": "Farmer created successfully",
+        "data": farmer
+    }
